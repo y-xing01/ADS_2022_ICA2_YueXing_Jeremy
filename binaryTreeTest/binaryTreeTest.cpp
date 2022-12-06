ef5121a91b2b1873faee58e9dad8e21ca93bebc3
YueXingTong <d00234628@student.dkit.ie>
2022-12-06 16:31:39 +0000

    :white_check_mark: Created Test Case for removing node with one child

--- binaryTreeTest/binaryTreeTest.cpp
+++ binaryTreeTest/binaryTreeTest.cpp
@@ -129,7 +129,6 @@ namespace binaryTreeTest
 		     / \ / \
 		    1  3 5  7
 		*/
-
 		TEST_METHOD(TestRemoveLeafNode)
 		{
 			BinaryTree<int, int> tree;
@@ -152,6 +151,38 @@ namespace binaryTreeTest
 			Assert::AreEqual(3, left->getRight()->getItem());
 			Assert::IsNull(right->getLeft());
 			Assert::AreEqual(7, right->getRight()->getItem());
+		}
+
+		/*
+		Test the remove function to try and remove a node with one child to the right
+		to test this we will use the following tree
+				4
+			   / \
+			  2   6
+			 / \ / \
+			1  3 5  7
+		*/
+		TEST_METHOD(TestRemoveNodeWithOneChild)
+		{
+			BinaryTree<int, int> tree;
+			tree.add(4, 4);
+			tree.add(2, 2);
+			tree.add(6, 6);
+			tree.add(1, 1);
+			tree.add(5, 5);
+			tree.add(7, 7);
+			tree.add(8, 8);
+			Assert::AreEqual(7, tree.count());
+			tree.remove(7);
+			Assert::AreEqual(6, tree.count());
+			Assert::AreEqual(4, tree.root->getItem());
+			Assert::AreEqual(2, tree.root->getLeft()->getItem());
+			Assert::AreEqual(6, tree.root->getRight()->getItem());
+			TNode<int, int>* left = tree.root->getLeft();
+			TNode<int, int>* right = tree.root->getRight();
+			Assert::AreEqual(1, left->getLeft()->getItem());
+			Assert::AreEqual(5, right->getLeft()->getItem());
+			Assert::AreEqual(8, right->getRight()->getItem());
 		}
 	};
 }
