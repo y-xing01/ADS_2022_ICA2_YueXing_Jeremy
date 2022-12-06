78f08e5c8a9f09b97d0759f1cbbd2465f5846130
YueXingTong <d00234628@student.dkit.ie>
2022-12-06 10:42:36 +0000

    :memo: Added test file for TNode

---
+++ TNodeTest/TNodeTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TNodeTest
+{
+	TEST_CLASS(TNodeTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
