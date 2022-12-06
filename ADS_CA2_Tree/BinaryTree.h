0774a41505cf92fbbefb30f48a1d8813523fcd2e
YueXingTong <d00234628@student.dkit.ie>
2022-12-06 09:30:12 +0000

    Added BinaryTree function :tada:

--- ADS_CA2_Tree/BinaryTree.h
+++ ADS_CA2_Tree/BinaryTree.h
@@ -6,7 +6,20 @@ template <typename K, typename E>
 class BinaryTree
 {
 public:
+	TNode<K, E>* root;
+	BinaryTree();
+	void add(K item, E item);
+	bool remove(K item, E item);
+	void clear();
+	int count();
+
 	E* search(K key) {
 		return nullptr;
 	}
 };
+
+template <typename K, typename E>
+BinaryTree<K, E>::BinaryTree()
+{
+	root = nullptr;
+}
