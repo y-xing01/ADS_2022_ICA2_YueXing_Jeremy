ccde40bb6aaeb68d83a710d5ab93a43cf19a2d7f
YueXingTong <d00234628@student.dkit.ie>
2022-12-06 10:41:37 +0000

    :memo: Added test file for Binarytree

---
+++ binaryTreeTest/binaryTreeTest.cpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ADS_CA2_TREE/BinaryTree.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace binaryTreeTest
+{
+	TEST_CLASS(binaryTreeTest)
+	{
+	public:
+		
+		TEST_METHOD(TestAddToEmptyTree)
+		{
+			//BinaryTree<int, int> tree;
+			//tree.add(1, 1);
+			//Assert::IsNotNull(tree.root);
+			//Assert::AreEqual(1, tree.root->getKey());
+		}
+	};
+}
