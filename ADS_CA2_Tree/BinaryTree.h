e4f6183347b0b52168e88f07e9d1f5cc08d403a6
YueXingTong <d00234628@student.dkit.ie>
2022-12-06 09:37:30 +0000

    Added constructor and few functions for BinaryTree :hammer:

--- ADS_CA2_Tree/BinaryTree.h
+++ ADS_CA2_Tree/BinaryTree.h
@@ -23,3 +23,144 @@ BinaryTree<K, E>::BinaryTree()
 {
 	root = nullptr;
 }
+
+template <typename K, typename E>
+void BinaryTree<K, E>::add(K item, E item)
+{
+	if (root == nullptr)
+	{
+		root = new TNode<K, E>();
+		root->setItem(item);
+	}
+	else
+	{
+		root->add(item);
+	}
+}
+
+template <typename K, typename E>
+int BinaryTree<K, E>::count()
+{
+	if (root == nullptr)
+		return 0;
+	return root->count();
+}
+
+template <typename K, typename E>
+bool BinaryTree<K, E>::remove(K item, E item)
+{
+	TNode<K, E>* toBeRemoved = root;
+	TNode<K, E>* parent = nullptr;
+	bool found = false;
+
+	while (!found && toBeRemoved != nullptr)
+	{
+
+		if (toBeRemoved->getItem() == item)
+		{
+
+			found = true;
+		}
+		else
+		{
+			parent = toBeRemoved;
+			if (toBeRemoved->getItem() > item)
+			{
+				toBeRemoved = toBeRemoved->getLeft();
+			}
+			else
+			{
+				toBeRemoved = toBeRemoved->getRight();
+			}
+		}
+	}
+	if (!found)
+		return false;
+
+	if (toBeRemoved->getLeft() == nullptr || toBeRemoved->getRight() == nullptr)
+	{
+		BSTNode<T>* newChild;
+		if (toBeRemoved->getLeft() == nullptr)
+		{
+			newChild = toBeRemoved->getRight();
+		}
+		else
+		{
+			newChild = toBeRemoved->getLeft();
+		}
+		if (parent == nullptr)
+		{
+			root = newChild;
+		}
+		else if (parent->getLeft() == toBeRemoved)
+		{
+			parent->setLeft(newChild);
+		}
+		else
+		{
+			parent->setRight(newChild);
+		}
+		return true;
+	}
+
+	TNode<K, E>* smallestParent = toBeRemoved;
+	TNode<K, E>* smallest = toBeRemoved->getRight();
+	while (smallest->getLeft() != nullptr)
+	{
+		smallestParent = smallest;
+		smallest = smallest->getLeft();
+	}
+	toBeRemoved->setItem(smallest->getItem());
+	if (smallestParent == toBeRemoved)
+	{
+		smallestParent->setRight(smallest->getRight());
+	}
+	else
+	{
+		smallestParent->setLeft(smallest->getRight());
+	}
+
+}
+
+template <typename K, typename E>
+void BinaryTree<K, E>::addItemToArray(TNode<K, E>* node, int& pos, int* arr)
+{
+	if (node != nullptr)
+	{
+		addItemToArray(node->getLeft(), pos, arr);
+		arr[pos] = node->getItem();
+		pos++;
+		addItemToArray(node->getRight(), pos, arr);
+	}
+
+}
+
+template <typename K>
+K* BinaryTree<K>::toArray()
+{
+	K* arr = new K[root->count()];
+	int pos = 0;
+	addItemToArray(root, pos, arr);
+	return arr;
+}
+
+template <typename E>
+E* BinaryTree<E>::toArray()
+{
+	K* arr = new E[root->count()];
+	int pos = 0;
+	addItemToArray(root, pos, arr);
+	return arr;
+}
+
+template <typename K, typename E>
+void BinaryTree<K, E>::clear()
+{
+	delete root;
+	root = nullptr;
+}
+template <typename K, typename E>
+BinaryTree<K, E>::~BinaryTree()
+{
+	delete root;
+}
